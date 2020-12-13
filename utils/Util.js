ad80e0e484433a1c42ebec748d4c61c033ece200
Mizushima Shiba <haidarme@yahoo.co.id>
2020-12-13 21:20:38 +0700

    Fix fibonacci formula
    Signed-off-by: Mizushima Shiba <haidarme@yahoo.co.id>

--- utils/Util.js
+++ utils/Util.js
@@ -4,15 +4,11 @@ module.exports = class Util{
   }
 
   static fibonacci(n) {
-
-    const result = [0, 1];
-    for (var i = 2; i <= n; i++) {
-      const a = (i - 1);
-      const b = (i - 2);
-      result.push(a + b);
+    const result = [0, 1]
+    for (var i = 2; i < n; i++) {
+      result.push(result[i-2] + result[i-1])
     }
     return result.toString()
-  
   }
 
   static factorial(number){
